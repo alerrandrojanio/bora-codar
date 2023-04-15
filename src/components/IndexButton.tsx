c4d5bc49bdb4ac45375d4b565a47593ba472926c
alerrandro <alerrandro.janio07@gmail.com>
2023-04-15 09:53:53 -0300

    feat: menu

--- src/components/IndexButton.tsx
+++ src/components/IndexButton.tsx
@@ -9,7 +9,7 @@ export function IndexButton({ text, href }: IndexButtonProps) {
   return (
     <Link
       href={href}
-      className="w-full bg-violet-600 py-6 px-14 rounded-full flex justify-center text-white font-semibold text-xl hover:bg-violet-700"
+      className="w-full bg-violet-600 py-2 px-14 rounded-lg flex justify-center text-white font-semibold text-xl hover:bg-violet-700"
     >
       {text}
     </Link>
